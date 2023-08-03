a49bee4078fbfa354ecc9859657d040e8b21048f
kyouma <roman.cabalum@gmail.com>
2023-08-03 16:24:28 +0800

    removed api basic auth keys

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -3,14 +3,9 @@ import axios from "axios";
 export default class Requests {
   constructor() {
     this.url = process.env.REACT_APP_API_BASEURL;
-    this.auth = {
-      username: process.env.REACT_APP_API_USERNAME,
-      password: process.env.REACT_APP_API_PASSWORD,
-    };
   }
   async post(endpoint, body) {
     return await axios.post(`${this.url}${endpoint}`, body, {
-      auth: this.auth,
       headers: {
         "Content-Type": "application/json",
       },
